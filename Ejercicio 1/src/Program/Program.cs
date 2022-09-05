c9c012477886cbe64637cf262b5c6731a26e4f5e
Your Name <bruno.bugarin@correo.ucu.edu.uy>
2022-09-04 22:54:15 -0500

    confir

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -11,7 +11,7 @@ namespace SRP
             Book book2 = new Book("Pro C#", "Troelsen", "001-035");
             book1.Place = new Location("A", "7", "Mullin");
             book2.Place = new Location("B", "3", "Central");
-
+            //lskdjflaskjdfljasldjfalsjdfl
 
             Console.WriteLine($"El libro se encuentra en el edificio {book1.Place.Building} en la sección {book1.Place.Section} en la estanteria {book1.Place.Shelf} ");
         }
